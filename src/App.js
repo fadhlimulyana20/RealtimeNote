c553e961a9f4bdc76bf3de7cc15aeaf136b0a4e8
fadhlimulyana20 <baba.fadhli@gmail.com>
2020-09-06 10:41:32 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {  }
+  render() {
+    return (
+      <React.Fragment>
+        <div className="container">
+          <div className="d-flex justify-content-between py-4">
+            <div className="title">
+              <h3>Realtime <span className="font-weight-bold">NoteApp</span></h3>
+            </div>
+            <div className="action-button">
+              <button className="btn btn-secondary rounded-pill px-4 font-weight-bold">Create New Note</button>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
